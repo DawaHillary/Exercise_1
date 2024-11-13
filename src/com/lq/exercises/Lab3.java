869c535344284b748d15d2340f3cdc616c823fcf
Dawa Edina Hillary <dawa.hillary@outlook.com>
2024-11-13 05:41:27 -0800

    first commit

---
+++ src/com/lq/exercises/Lab3.java
@@ -0,0 +1,12 @@
+//Using Java Flow Control
+//1. Create a new class named Lab3 in the com.lq.exercises package. Ensure
+//that the new Lab3 class has a main() method. This can usually be added
+//automatically in the IDE when you are creating the class. Use the following as
+//a guide
+package com.lq.exercises;
+
+public class Lab3 {
+    public static void main(String[] args) {
+
+    }
+}
